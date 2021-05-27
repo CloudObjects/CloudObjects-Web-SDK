a7541a6e5ce1f6e2f72f56eea14f88f0fa4b91f0
LukasRos <lr@lukasrosenstock.net>
2021-05-27 16:36:21 +0200

    Support client-side redirect flow

--- src/COWebPlatform.ts
+++ src/COWebPlatform.ts
@@ -25,7 +25,29 @@ export class COWebPlatform {
             let session = JSON.parse(sessionStorage.getItem("COWebPlatformSessionData"));
             if (session.hasOwnProperty('aauid') && session.hasOwnProperty('accessToken'))
                 this.setSignedInWithAccountDetails(session.aauid, session.accessToken);
-        };
+        } else
+        if (sessionStorage.getItem("COWebPlatformFlowState") !== null) {            
+            // Parse query for returned parameters
+            let query = {};
+            let pairs = document.location.search.substr(1).split('&');
+            for (var i = 0; i < pairs.length; i++) {
+                let pair = pairs[i].split('=');
+                query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+            }
+
+            query['state'] = sessionStorage.getItem("COWebPlatformFlowState"); 
+                
+            // Attempt to complete flow
+            let webApp = this;
+            this.platformClient.post('signInWithCode', query)
+                .then(function(apiResponse) {
+                    if (apiResponse.data.hasOwnProperty('type') && apiResponse.data.type == 'account') {
+                        let content = apiResponse.data.content;
+                        webApp.setSignedInWithAccountDetails(content.aauid, content.access_token);
+                    }
+                });
+            sessionStorage.removeItem("COWebPlatformFlowState");
+        }
     }
 
     private signInPreCheck(identityProviderCOID? : COID) {
@@ -41,16 +63,37 @@ export class COWebPlatform {
 
     }
 
-    signInWithRedirect(identityProviderCOID : COID) {
+    async signInWithRedirect(identityProviderCOID : COID, parameters : Object) : Promise<any> {
         this.signInPreCheck(identityProviderCOID);
+        
+        if (typeof(parameters) !== "object")
+            parameters = {};
+        if (!parameters.hasOwnProperty('redirect_uri'))
+            parameters['redirect_uri'] = document.location.href;
 
-        document.location.href = 'https://webplatform.co-n.net/'
-            + this.appCOID.getNamespace() + '/' + this.appCOID.getNameSegment()
-            + '/signInWith/' + identityProviderCOID.getNamespace()
-            + '?response_type=code&redirect_uri=';
+        parameters['response_type'] = 'code';
+
+        let apiResponse = await this.platformClient.post(
+            'signInWith/' + identityProviderCOID.getNamespace(),
+            parameters
+        );
+
+        if (apiResponse.data.hasOwnProperty('provider_response')
+                && apiResponse.data.hasOwnProperty('state')
+                && apiResponse.data.provider_response.hasOwnProperty('redirect_uri')) {
+            // Save state
+            sessionStorage.setItem("COWebPlatformFlowState", apiResponse.data.state);
+            // Redirect to provider
+            document.location.href = apiResponse.data.provider_response.redirect_uri;
+        } else
+            return apiResponse.data;
     }
 
-    async signInWithCredentials(identityProviderCOID : COID, credentials : Array<any>) : Promise<any> {
+    /*async signInWithPopup(identityProviderCOID : COID, credentials : Array<any>) {
+
+    } */
+
+    async signInWithCredentials(identityProviderCOID : COID, credentials : Object) : Promise<any> {
         this.signInPreCheck(identityProviderCOID);
 
         let apiResponse = await this.platformClient.post(
